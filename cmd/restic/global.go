29e3870706125abb60f44b7b9feca682d182f209
Alexander Neumann <alexander@bumpern.de>
2017-07-16 19:00:11 +0200

    Use proper cache directory for integration tests

--- cmd/restic/global.go
+++ cmd/restic/global.go
@@ -39,6 +39,7 @@ type GlobalOptions struct {
 	Quiet        bool
 	NoLock       bool
 	JSON         bool
+	CacheDir     string
 
 	ctx      context.Context
 	password string
@@ -70,6 +71,7 @@ func init() {
 	f.BoolVar(&globalOptions.NoLock, "no-lock", false, "do not lock the repo, this allows some operations on read-only repos")
 	f.BoolVarP(&globalOptions.JSON, "json", "", false, "set output mode to JSON for commands that support it")
 
+	f.StringVar(&globalOptions.CacheDir, "cache-dir", "", "set the cache directory")
 	f.StringSliceVarP(&globalOptions.Options, "option", "o", []string{}, "set extended option (`key=value`, can be specified multiple times)")
 
 	restoreTerminal()
@@ -323,7 +325,7 @@ func OpenRepository(opts GlobalOptions) (*repository.Repository, error) {
 		return nil, err
 	}
 
-	cache, err := cache.New(s.Config().ID, "")
+	cache, err := cache.New(s.Config().ID, opts.CacheDir)
 	if err != nil {
 		Warnf("unable to open cache: %v\n", err)
 	} else {
